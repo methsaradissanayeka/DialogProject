bd390f8c015b615f2ed09c7bd397f2dd924357ec
Methsara Dissanayeka <methsara@me.com>
2023-09-20 11:49:17 +0530

    Updated fixing bug 1.1

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-import csv
-from tabulate import tabulate
+import pandas as pd
 
 file_path = '/Users/methsara/Downloads/MDT 20230817/Huawei MDT.csv'
 
 try:
-    with open(file_path, mode='r', encoding='utf-8') as csv_file:
-        csv_reader = csv.reader(csv_file)
+    # Read the CSV file into a pandas DataFrame
+    df = pd.read_csv(file_path, encoding='utf-8')
 
-        # Read and display the first 10 rows as a sample
-        data = [row for i, row in enumerate(csv_reader) if i < 10]
-
-        if data:
-            print(tabulate(data, headers="firstrow", tablefmt="grid"))
-        else:
-            print("No data found in the CSV file.")
+    # Display the DataFrame
+    print(df)
 
 except Exception as e:
     print(f"An error occurred: {str(e)}")
